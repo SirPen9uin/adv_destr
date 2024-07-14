70da74b00dab36f163427e66e81c5637ce7e3458
Pen9uin <sirpen9uin@gmail.com>
2024-07-14 12:22:07 +0300

    done

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,21 @@ import sum from './basic';
 console.log('worked');
 
 console.log(sum([1, 2]));
+
+export default function orderByProps(object, arr) {
+  const objFirst = [];
+  const objSecond = [];
+
+  for (const key in object) {
+    if (arr.includes(key)) {
+      objFirst.push({ key, value: object[key] });
+    } else {
+      objSecond.push({ key, value: object[key] });
+    }
+  }
+
+  objFirst.sort((a, b) => arr.indexOf(a.key) - arr.indexOf(b.key));
+  objSecond.sort((a, b) => (a.key > b.key ? 1 : -1));
+
+  return [...objFirst, ...objSecond];
+}
